bf8997e67ad8eb1176ce5fd24d40f2c2d08ee037
abbeydmarketer-wq <abbeydmarketer@gmail.com>
2025-09-20 17:02:24 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  server: {
-    host: "0.0.0.0",
-    port: 5000,
-    strictPort: true,
-    hmr: {
-      clientPort: 443
-    }
-  }
+  base: "./"
 })
